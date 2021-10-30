79f82d1dc94eef5c37cf78f29e568ab63cad4402
mvisic <marcusvisic@yahoo.com.au>
2021-10-30 22:19:51 +0800

    print-date

---
+++ Python/print-date.py
@@ -0,0 +1,7 @@
+from datetime import date
+
+# Read the current date
+current_date = date.today()
+
+# Print the formatted date
+print("Today is :%d-%d-%d" % (current_date.day,current_date.month,current_date.year))
